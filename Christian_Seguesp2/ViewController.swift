9318350e147dc46dca8ea9691b68702149c39657
Christian Severin <severinchristian25@gmail.com>
2021-04-13 11:14:08 -0400

    Initial Commit

---
+++ Christian_Seguesp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Christian_Seguesp2
+//
+//  Created by Christian Severin on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
