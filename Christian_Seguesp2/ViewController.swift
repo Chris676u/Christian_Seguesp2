d819c2221fafb4ab6789e8e4bb267f1fb54ecc4f
Christian Severin <severinchristian25@gmail.com>
2021-04-15 13:36:30 -0400

    More Sophisticated...

--- Christian_Seguesp2/ViewController.swift
+++ Christian_Seguesp2/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+      
+    @IBAction func LogInButton(_ sender: UIButton) { if segueSwitch.isOn { performSegue(withIdentifier: "LogIn", sender: nil)
+        }
+    }
+    @IBAction func LogOut(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    @IBAction func SignUpButton(_ sender: UIButton) { if segueSwitch.isOn { performSegue(withIdentifier: "SignUp", sender: nil) }
+    }
 }
 
